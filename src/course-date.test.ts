c35349095ab895da78db7cef622d52310059ed93
James Geer <jamesrgeer@hotmail.co.uk>
2022-04-17 17:56:44 +0100

    finally got jest to work with async api call

--- src/course-date.test.ts
+++ src/course-date.test.ts
@@ -1,6 +1,10 @@
+import 'isomorphic-fetch';
+
 // @ts-ignore
 const courseDate = require('./course-date');
 
 test('should return course created date', () => {
-    expect(new courseDate().getCreatedDate("1309202")).toBe("2017-07-31T21:29:36Z");
+    expect.assertions(1);
+    return expect(new courseDate().getCreatedDate("1309202"))
+        .resolves.toEqual("2017-07-31T21:29:36Z");
 });
