f86abd46f67b428b467b16a50e52fcaba0d2511a
James Geer <jamesrgeer@hotmail.co.uk>
2022-04-17 17:26:51 +0100

    removed dummy test, now trying to get class test to work

--- src/course-date.test.ts
+++ src/course-date.test.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-const sum = require('./course-date');
+const courseDate = require('./course-date');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('should return course created date', () => {
+    expect(new courseDate().getCreatedDate("1309202")).toBe("2017-07-31T21:29:36Z");
 });
