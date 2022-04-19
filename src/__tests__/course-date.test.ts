ff6212f3b8b98cfce666b1880b0335d3d609833d
James Geer <jamesrgeer@hotmail.co.uk>
2022-04-19 17:34:40 +0100

    setup prettier, not sure if I like it or not. test finally working

--- src/__tests__/course-date.test.ts
+++ src/__tests__/course-date.test.ts
@@ -1,53 +1,57 @@
-import 'isomorphic-fetch';
+import "isomorphic-fetch";
 import { CourseDate } from "../classes/course-date";
-import {format} from 'prettier'
+import { format } from "prettier";
 
-test('body should contain course id', () => {
-    document.body = document.createElement('body');
-    document.body.setAttribute('data-clp-course-id', '1309202')
+test("body should contain course id", () => {
+	document.body = document.createElement("body");
+	document.body.setAttribute("data-clp-course-id", "1309202");
 
-    expect(new CourseDate().getCourseId()).toEqual('1309202');
+	expect(new CourseDate().getCourseId()).toEqual("1309202");
 });
 
-test('real api should return course created date', () => {
-    expect.assertions(1);
-    return expect(new CourseDate().getCreatedDate("1309202"))
-        .resolves.toEqual("2017-07-31T21:29:36Z");
+test("real api should return course created date", () => {
+	expect.assertions(1);
+	return expect(new CourseDate().getCreatedDate("1309202")).resolves.toEqual(
+		"2017-07-31T21:29:36Z"
+	);
 });
 
-test('mock api should return course created date', () => {
-    expect.assertions(1);
+test("mock api should return course created date", () => {
+	expect.assertions(1);
 
-    global.fetch = jest.fn(() =>
-        Promise.resolve({
-            json: () => Promise.resolve({ created: "2017-07-31T21:29:36Z" })
-        })
-    ) as jest.Mock;
+	global.fetch = jest.fn(() =>
+		Promise.resolve({
+			json: () => Promise.resolve({ created: "2017-07-31T21:29:36Z" }),
+		})
+	) as jest.Mock;
 
-    return expect(new CourseDate().getCreatedDate("1309202"))
-        .resolves.toEqual("2017-07-31T21:29:36Z");
+	return expect(new CourseDate().getCreatedDate("1309202")).resolves.toEqual(
+		"2017-07-31T21:29:36Z"
+	);
 });
 
-test('mock api should return course not found', () => {
-    expect.assertions(1);
+test("mock api should return course not found", () => {
+	expect.assertions(1);
 
-    global.fetch = jest.fn(() =>
-        Promise.resolve({
-            json: () => Promise.resolve({ detail: "Not found." })
-        })
-    ) as jest.Mock;
+	global.fetch = jest.fn(() =>
+		Promise.resolve({
+			json: () => Promise.resolve({ detail: "Not found." }),
+		})
+	) as jest.Mock;
 
-    return expect(new CourseDate().getCreatedDate("4000050505"))
-        .resolves.toEqual("");
+	return expect(
+		new CourseDate().getCreatedDate("4000050505")
+	).resolves.toEqual("");
 });
 
-test('utc datetime should convert to mm/yyyy', () => {
-    expect(new CourseDate().formatUTCDateTime('2017-07-31T21:29:36Z'))
-        .toEqual('07/2017');
+test("utc datetime should convert to mm/yyyy", () => {
+	expect(new CourseDate().formatUTCDateTime("2017-07-31T21:29:36Z")).toEqual(
+		"07/2017"
+	);
 });
 
-test('created date HTML should match', () => {
-    const expectedHTML = `
+test("created date HTML should match", () => {
+	const expectedHTML = `
         <div class="clp-lead__element-item">
             <div class="last-update-date" data-purpose="last-update-date">
                 <svg aria-hidden="true" focusable="false" class="udlite-icon udlite-icon-xsmall udlite-icon-color-neutral last-update-date__icon">
@@ -56,26 +60,25 @@ test('created date HTML should match', () => {
                 <span>Created 07/2017</span>
             </div>
         </div>
-    `
+    `;
 
-    expect(new CourseDate().getCreatedDateHTML('07/2017'))
-        .toEqual(expectedHTML);
+	expect(new CourseDate().getCreatedDateHTML("07/2017")).toEqual(
+		expectedHTML
+	);
 });
 
-test('appended created date HTML appears in the correct location', () => {
-    document.body.innerHTML +=
-        `<div class="clp-lead__element-meta">
+test("appended created date HTML appears in the correct location", () => {
+	document.body.innerHTML += `<div class="clp-lead__element-meta">
             <div class="clp-lead__element-item">Last updated 04/2022</div>
             <div class="clp-lead__element-item">English</div>
             <div class="clp-lead__element-item">English [Auto]</div>
-        </div>`
+        </div>`;
 
-    const courseDate = new CourseDate();
-    const createdDateHTML = courseDate.getCreatedDateHTML('07/2017');
-    courseDate.appendCreatedDateHTML(createdDateHTML);
+	const courseDate = new CourseDate();
+	const createdDateHTML = courseDate.getCreatedDateHTML("07/2017");
+	courseDate.appendCreatedDateHTML(createdDateHTML);
 
-    const expectedHTML =
-        `<div class="clp-lead__element-meta">
+	const expectedHTML = `<div class="clp-lead__element-meta">
             <div class="clp-lead__element-item">
                 <div class="last-update-date" data-purpose="last-update-date">
                     <svg aria-hidden="true" focusable="false" class="udlite-icon udlite-icon-xsmall udlite-icon-color-neutral last-update-date__icon">
@@ -84,15 +87,18 @@ test('appended created date HTML appears in the correct location', () => {
                     <span>Created 07/2017</span>
                 </div>
             </div>
+            
             <div class="clp-lead__element-item">Last updated 04/2022</div>
             <div class="clp-lead__element-item">English</div>
             <div class="clp-lead__element-item">English [Auto]</div>
-        </div>`
+        </div>`;
 
-    const selectorResult = document.querySelector('.clp-lead__element-meta').outerHTML;
+	const selectorResult = document.querySelector(
+		".clp-lead__element-meta"
+	).outerHTML;
 
-    // const formatStmt = (str: string) => format(str, { parser: 'html' })
-    // expect(formatStmt(selectorResult)).toBe(formatStmt(expectedHTML));
+	const formatStmt = (str: string) => format(str, { parser: "html" });
+	expect(formatStmt(selectorResult)).toBe(formatStmt(expectedHTML));
 
-    expect(selectorResult).toEqual(expectedHTML);
+	// expect(selectorResult).toEqual(expectedHTML);
 });
--- src/classes/course-date.ts
+++ src/classes/course-date.ts
@@ -1,60 +1,61 @@
 interface UdemyJSON {
-    id: number;
-    created: string;
+	id: number;
+	created: string;
 }
 
 class CourseDate {
+	constructor() {
+		//this.init()
+	}
 
-    constructor() {
-        //this.init()
-    }
+	private init() {
+		const courseId = this.getCourseId();
+	}
 
-    private init() {
-        const courseId = this.getCourseId();
-    }
+	public getCourseId(): string {
+		return document.body.getAttribute("data-clp-course-id");
+	}
 
-    public getCourseId(): string {
-        return document.body.getAttribute('data-clp-course-id');
-    }
+	public async getCreatedDate(courseId: string): Promise<string> {
+		const url =
+			"https://www.udemy.com/api-2.0/courses/" +
+			courseId +
+			"/?fields[course]=created";
 
-    public async getCreatedDate(courseId: string): Promise<string> {
-        const url = 'https://www.udemy.com/api-2.0/courses/' + courseId + '/?fields[course]=created';
+		try {
+			const response = await fetch(url);
+			const json = (await response.json()) as UdemyJSON;
 
-        try {
-            const response = await fetch(url);
-            const json = await response.json() as UdemyJSON;
+			if (json.created) {
+				return json.created;
+			}
+		} catch (e) {
+			return "";
+		}
 
-            if(json.created) {
-                return json.created;
-            }
+		return "";
+	}
 
-        } catch (e) {
-            return ""
-        }
+	public formatUTCDateTime(utcDateTime: string): string {
+		if (!utcDateTime.includes("Z")) {
+			return "";
+		}
 
-        return "";
-    }
+		let formattedDate = "";
+		const date = new Date(utcDateTime);
+		const month = date.getMonth() + 1;
 
-    public formatUTCDateTime(utcDateTime: string): string {
-        if(!utcDateTime.includes('Z')) {
-            return "";
-        }
+		if (month < 10) {
+			formattedDate += "0";
+		}
 
-        let formattedDate = '';
-        const date = new Date(utcDateTime);
-        const month = date.getMonth() + 1;
+		formattedDate += month + "/" + date.getFullYear();
 
-        if(month < 10) {
-            formattedDate += '0'
-        }
+		return formattedDate;
+	}
 
-        formattedDate += month + '/' + date.getFullYear();
-
-        return formattedDate;
-    }
-
-    public getCreatedDateHTML(createdDate: string): string {
-        return `
+	public getCreatedDateHTML(createdDate: string): string {
+		return `
         <div class="clp-lead__element-item">
             <div class="last-update-date" data-purpose="last-update-date">
                 <svg aria-hidden="true" focusable="false" class="udlite-icon udlite-icon-xsmall udlite-icon-color-neutral last-update-date__icon">
@@ -63,20 +64,18 @@ class CourseDate {
                 <span>Created ${createdDate}</span>
             </div>
         </div>
-        `
-    }
+        `;
+	}
 
-    public appendCreatedDateHTML(createdDateHTML: string): void {
-        const target = document.querySelector('.clp-lead__element-meta');
-        target.insertAdjacentHTML('afterbegin', createdDateHTML);
-        //const node = new DOMParser().parseFromString(createdDateHTML, 'text/html');
+	public appendCreatedDateHTML(createdDateHTML: string): void {
+		const target = document.querySelector(".clp-lead__element-meta");
+		target.insertAdjacentHTML("afterbegin", createdDateHTML);
+		//const node = new DOMParser().parseFromString(createdDateHTML, 'text/html');
 
-        //console.log(node)
+		//console.log(node)
 
-        //target.prepend(node);
-
-    }
-
+		//target.prepend(node);
+	}
 }
 
 export { CourseDate };
