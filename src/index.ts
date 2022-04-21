75cdb58423d0eb079f414086be20acddef97606d
James Geer <jamesrgeer@hotmail.co.uk>
2022-04-21 07:45:07 +0100

    browser api to detect tab updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { CreatedDate } from "./classes/CreatedDate";
 
-if (window.location.host === "www.udemy.com") {
-	const pathArray = window.location.pathname.split("/").filter(Boolean);
-	if (pathArray[0] === "course") {
-		new CreatedDate().run();
+declare const browser: any;
+
+const coursePage = () => {
+	if (window.location.host === "www.udemy.com") {
+		const pathArray = window.location.pathname.split("/").filter(Boolean);
+		if (pathArray[0] === "course") {
+			new CreatedDate().run().then();
+		}
 	}
-}
+};
+
+browser.tabs.onUpdated.addListener(coursePage);
