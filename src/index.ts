c9f69d8c85ef0c4f643dc341654811eda5778f63
James Geer <jamesrgeer@hotmail.co.uk>
2022-04-22 19:46:39 +0100

    going to remove bundler and try webpack

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+import browser from "webextension-polyfill";
 import { CreatedDate } from "./classes/CreatedDate";
 
-document.onreadystatechange = function () {
-	console.log(document.readyState);
-	if (document.readyState === "complete") {
-		console.log(document.readyState);
+console.log("test");
+
+// browser.tabs.onUpdated.addListener(() => {
+// 	document.onreadystatechange = function () {
+// 		console.log(document.readyState);
+// 		if (document.readyState === "complete") {
+// 			console.log(document.readyState);
+// 			new CreatedDate().run().then();
+// 			console.log("hello there");
+// 		}
+// 	};
+// });
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+	if (changeInfo.url) {
+		console.log("Tab: " + tabId + " URL changed to " + changeInfo.url);
+
+		// document.onreadystatechange = function () {
+		// 	console.log(document.readyState);
+		// 	if (document.readyState === "complete") {
+		// 		console.log(document.readyState);
 		new CreatedDate().run().then();
 		console.log("hello there");
+		// }
+		// };
 	}
-};
+}
+
+browser.tabs.onUpdated.addListener(handleUpdated);
