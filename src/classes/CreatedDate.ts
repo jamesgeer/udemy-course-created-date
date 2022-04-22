92a9e59308fb3e765f8ed6dda2ef2c5053ec72bc
James Geer <jamesrgeer@hotmail.co.uk>
2022-04-22 12:05:20 +0100

    trying to get anything to work

--- src/classes/CreatedDate.ts
+++ src/classes/CreatedDate.ts
@@ -85,3 +85,5 @@ export class CreatedDate {
 		target.insertAdjacentHTML("afterbegin", createdDateHTML);
 	}
 }
+
+new CreatedDate().run().then();
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-import { CreatedDate } from "./classes/CreatedDate";
 import browser from "webextension-polyfill";
 
-const coursePage = (tabId, changeInfo, tab) => {
-	console.log("Hello there");
-	console.log(tab.url);
+// const coursePage = (tabId, changeInfo, tab) => {
+// 	console.log("Hello there");
+// 	console.log(tab.url);
+//
+// 	if (window.location.host === "www.udemy.com") {
+// 		console.log("Looks like we're on Udemy");
+//
+// 		const pathArray = window.location.pathname.split("/").filter(Boolean);
+// 		if (pathArray[0] === "course") {
+// 			console.log("Looking at a course, are we?");
+// 			new CreatedDate().run().then();
+// 		}
+// 	}
+// };
+//
+// browser.tabs.onUpdated.addListener(coursePage);
 
-	if (window.location.host === "www.udemy.com") {
-		console.log("Looks like we're on Udemy");
+const executing = browser.tabs.executeScript({
+	file: "index.js",
+});
 
-		const pathArray = window.location.pathname.split("/").filter(Boolean);
-		if (pathArray[0] === "course") {
-			console.log("Looking at a course, are we?");
-			new CreatedDate().run().then();
-		}
-	}
-};
-
-browser.tabs.onUpdated.addListener(coursePage);
+executing.then();
